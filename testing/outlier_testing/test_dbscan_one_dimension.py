37639a23d7f91431544537c483ee320109e2aa11
chrisvieira <christophervieira1199@gmail.com>
2021-09-17 13:48:08 -0400

    #14 Change testing files for test format

--- testing/outlier_testing/test_dbscan_one_dimension.py
+++ testing/outlier_testing/test_dbscan_one_dimension.py
@@ -3,46 +3,49 @@ from helper_functions.outlier_handlers import identify_outliers
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Tests the z score outlier removal
-np.random.seed(10)
 
-mu, sigma = 0, 1  # mean and standard deviation
-mu_outliers, sigma_outliers = 20, 1  # mean and standard deviation
-s = np.random.normal(mu, sigma, 100)
-outliers = np.random.normal(mu_outliers, sigma_outliers, 4)
-s = np.append(s, outliers)
+def test_method1():
+    # Tests the z score outlier removal
+    np.random.seed(10)
 
-intervals = []
-interval_increment = 0.25
+    mu, sigma = 0, 1  # mean and standard deviation
+    mu_outliers, sigma_outliers = 20, 1  # mean and standard deviation
+    s = np.random.normal(mu, sigma, 100)
+    outliers = np.random.normal(mu_outliers, sigma_outliers, 4)
+    s = np.append(s, outliers)
 
-accumulator = min(s) - interval_increment
-max_limit = max(s) + 2 * interval_increment
+    intervals = []
+    interval_increment = 0.25
 
-while accumulator < max_limit:
-    intervals.append(accumulator)
-    accumulator = accumulator + interval_increment
+    accumulator = min(s) - interval_increment
+    max_limit = max(s) + 2 * interval_increment
 
-data_df = pd.DataFrame(s, columns=["Data"])
+    while accumulator < max_limit:
+        intervals.append(accumulator)
+        accumulator = accumulator + interval_increment
 
-# plt.hist(data_df["Data"].values, bins=intervals, color="blue")
-# plt.title("Original Data Set")
-# plt.xlabel("Data Point Value")
-# plt.ylabel("Data Point Frequency")
-# plt.show()
+    data_df = pd.DataFrame(s, columns=["Data"])
 
-method_details = {"method_name": "dbscan", "is_univariate": False, "algorithm": "auto", "eps": 0.5, "min_samples": 5}
+    # plt.hist(data_df["Data"].values, bins=intervals, color="blue")
+    # plt.title("Original Data Set")
+    # plt.xlabel("Data Point Value")
+    # plt.ylabel("Data Point Frequency")
+    # plt.show()
 
-# print(identify_outliers.return_outliers(data_df=data_df, method_details=method_details))
-non_outliers, outliers = identify_outliers.return_outliers(data_df=data_df, method_details=method_details)
+    method_details = {"method_name": "dbscan", "is_univariate": False, "algorithm": "auto", "eps": 0.5,
+                      "min_samples": 5}
 
-# plt.hist(non_outliers["Data"].values, bins=intervals, color="green")
-# plt.hist(outliers["Data"].values, bins=intervals, color="red")
-# plt.title("Outliers Detected From Original Set")
-# plt.xlabel("Data Point Value")
-# plt.ylabel("Data Point Frequency")
-# plt.show()
+    # print(identify_outliers.return_outliers(data_df=data_df, method_details=method_details))
+    non_outliers, outliers = identify_outliers.return_outliers(data_df=data_df, method_details=method_details)
 
-assert len(non_outliers["Data"].values) == 100
-assert len(outliers["Data"].values) == 4
+    # plt.hist(non_outliers["Data"].values, bins=intervals, color="green")
+    # plt.hist(outliers["Data"].values, bins=intervals, color="red")
+    # plt.title("Outliers Detected From Original Set")
+    # plt.xlabel("Data Point Value")
+    # plt.ylabel("Data Point Frequency")
+    # plt.show()
 
-# exit(0)
+    assert len(non_outliers["Data"].values) == 100
+    assert len(outliers["Data"].values) == 4
+
+    # exit(0)
--- testing/outlier_testing/test_dbscan_two_dimension.py
+++ testing/outlier_testing/test_dbscan_two_dimension.py
@@ -3,47 +3,50 @@ from helper_functions.outlier_handlers import identify_outliers
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Tests the z score outlier removal
-np.random.seed(10)
 
-mu, sigma = 0, 1  # mean and standard deviation
-mu_outliers, sigma_outliers = 20, 1  # mean and standard deviation
-s_x = np.random.normal(mu, sigma, 100)
-s_y = np.random.normal(mu, sigma, 100)
-outliers_x = np.random.normal(mu_outliers, sigma_outliers, 4)
-outliers_y = np.random.normal(mu_outliers, sigma_outliers, 4)
-s_x = np.append(s_x, outliers_x)
-s_y = np.append(s_y, outliers_y)
+def test_method1():
+    # Tests the z score outlier removal
+    np.random.seed(10)
 
-# print(s_x)
-# print(s_y)
+    mu, sigma = 0, 1  # mean and standard deviation
+    mu_outliers, sigma_outliers = 20, 1  # mean and standard deviation
+    s_x = np.random.normal(mu, sigma, 100)
+    s_y = np.random.normal(mu, sigma, 100)
+    outliers_x = np.random.normal(mu_outliers, sigma_outliers, 4)
+    outliers_y = np.random.normal(mu_outliers, sigma_outliers, 4)
+    s_x = np.append(s_x, outliers_x)
+    s_y = np.append(s_y, outliers_y)
 
-combined_list = np.append(s_x, s_y)
-combined_list = combined_list.reshape(-1, 2)
+    # print(s_x)
+    # print(s_y)
 
-# print(combined_list)
+    combined_list = np.append(s_x, s_y)
+    combined_list = combined_list.reshape(-1, 2)
 
-data_df = pd.DataFrame(combined_list, columns=["Data X", "Data Y"])
+    # print(combined_list)
 
-# plt.scatter(data_df["Data X"].values, data_df["Data Y"], color="blue")
-# plt.title("Original Data Set")
-# plt.xlabel("Data Point X Value")
-# plt.ylabel("Data Point Y Value")
-# plt.show()
+    data_df = pd.DataFrame(combined_list, columns=["Data X", "Data Y"])
 
-method_details = {"method_name": "dbscan", "is_univariate": False, "algorithm": "auto", "eps": 0.5, "min_samples": 5}
+    # plt.scatter(data_df["Data X"].values, data_df["Data Y"], color="blue")
+    # plt.title("Original Data Set")
+    # plt.xlabel("Data Point X Value")
+    # plt.ylabel("Data Point Y Value")
+    # plt.show()
 
-# print(identify_outliers.return_outliers(data_df=data_df, method_details=method_details))
-non_outliers, outliers = identify_outliers.return_outliers(data_df=data_df, method_details=method_details)
+    method_details = {"method_name": "dbscan", "is_univariate": False, "algorithm": "auto", "eps": 0.5,
+                      "min_samples": 5}
 
-# plt.scatter(non_outliers["Data X"].values, non_outliers["Data Y"], color="green")
-# plt.scatter(outliers["Data X"].values, outliers["Data Y"], color="red")
-# plt.title("Outliers Detected From Original Set")
-# plt.xlabel("Data Point X Value")
-# plt.ylabel("Data Point Y Value")
-# plt.show()
+    # print(identify_outliers.return_outliers(data_df=data_df, method_details=method_details))
+    non_outliers, outliers = identify_outliers.return_outliers(data_df=data_df, method_details=method_details)
 
-assert len(non_outliers["Data X"].values) == 80
-assert len(outliers["Data X"].values) == 24
+    # plt.scatter(non_outliers["Data X"].values, non_outliers["Data Y"], color="green")
+    # plt.scatter(outliers["Data X"].values, outliers["Data Y"], color="red")
+    # plt.title("Outliers Detected From Original Set")
+    # plt.xlabel("Data Point X Value")
+    # plt.ylabel("Data Point Y Value")
+    # plt.show()
 
-# exit(0)
+    assert len(non_outliers["Data X"].values) == 80
+    assert len(outliers["Data X"].values) == 24
+
+    # exit(0)
